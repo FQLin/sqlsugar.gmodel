b3571ba5c3881dbf895e5bd6cee022425d9c7f02
FQLin <fanqinglin1990@hotmail.com>
2020-05-22 21:24:07 +0800

    helper

--- src/util/Helper.ts
+++ src/util/Helper.ts
@@ -1,12 +1,5 @@
 namespace Helper{
-    isPlainObject:(obj:any):boolean=>{
+    export const isPlainObject = (obj:any):boolean=>{
         return typeof(obj) === "object";
-    }
-    export isPlainObject(obj:any):boolean{
-        if(typeof(obj) === "objet"){
-            return true;
-        }else{
-            return false;
-        }
     }
 }
